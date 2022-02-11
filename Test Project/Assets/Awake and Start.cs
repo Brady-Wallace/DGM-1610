e2c38e8f04ef9d8ddfec4995f00f4ba3a30acc4c
Brady <bw030203@gmail.com>
2022-02-10 21:40:00 -0700

    Added Awake and Start

---
+++ Test Project/Assets/Awake and Start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    // Update is called once per frame
+    void Start ()
+    {
+        Debug.Log("Start called.");
+    }
+}
