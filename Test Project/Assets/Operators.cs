2f53b2ea7ee6ee06f795aea12bfe71e9e5fc9523
Brady <bw030203@gmail.com>
2022-02-17 22:22:43 -0700

    Played around with operators

---
+++ Test Project/Assets/Operators.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    int x = 10;
+    x++;
+    int y = 10;
+    y += 5
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log(10 * 5)
+       Debug.Log(10 / 5)
+       Debug.Log(x)
+       Debug.Log(y)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
