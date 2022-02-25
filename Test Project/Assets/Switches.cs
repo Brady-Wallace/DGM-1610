0c1b2e97e1ec9b4a073916675f99657085f37f94
Brady <bw030203@gmail.com>
2022-02-24 23:30:52 -0700

    Added more switches to the script

--- Test Project/Assets/Switches.cs
+++ Test Project/Assets/Switches.cs
@@ -6,6 +6,7 @@ public class Switches : MonoBehaviour
 {
     int day = 2;
     int day_of_the_week = 3;
+    string name = "Dave";
     void Start()
     {
         switch (day)
@@ -17,7 +18,7 @@ public class Switches : MonoBehaviour
                 Debug.Log("Tuesday");
                 break;
         }
-        switch(day_of_the_week)
+        switch (day_of_the_week)
         {
             case 1:
                 Debug.Log("Today is Saturday");
@@ -28,6 +29,37 @@ public class Switches : MonoBehaviour
             default:
                 Debug.Log("Looking forward to the Weekend");
                 break;
+        }
+        switch (name)
+        {
+            case "Bill":
+                Debug.Log("Name is Bill");
+                break;
+            case "Bob":
+                Debug.Log("Name is Bob");
+                break;
+            case "Dave":
+                Debug.Log("Name is Dave");
+                break;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+        switch (i)
+            {
+                case 0:
+                    Debug.Log("i is equal to 0");
+                    break;
+                case 1:
+                    Debug.Log("i is equal to 1");
+                    break;
+                case 2:
+                    Debug.Log("i is equal to 2");
+                    break;
+                case 3:
+                    Debug.Log("i is equal to 3");
+                    break;
+            }
+
         }
     
     
