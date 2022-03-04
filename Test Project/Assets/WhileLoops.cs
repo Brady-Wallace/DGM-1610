b3e360254e28117e38f2c4ece8df9c781e22098b
Brady <bw030203@gmail.com>
2022-03-03 19:03:12 -0700

    Added more loops

--- Test Project/Assets/WhileLoops.cs
+++ Test Project/Assets/WhileLoops.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class WhileLoops : MonoBehaviour
 {
     int i = 1;
+    int x = 1;
+    bool y = true;
+    int health = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,30 @@ public class WhileLoops : MonoBehaviour
 
         do
         {
-            Debug.Log(i);
-            i++;
+            Debug.Log(x);
+            x++;
         }
-        while (i < 6);
+        while (x < 6);
 
+        while (y)
+        {
+            Debug.Log("hello");
+            if (y)
+            {
+                break;
+            }
+        }
+
+        while (health > 0)
+        {
+            Debug.Log("Player has " + health + " health!");
+            if (health == 1)
+            {
+                Debug.Log("Player is almost dead!!!");
+            }
+            health--;
+        }
+        Debug.Log("Game Over");
     }
 
     // Update is called once per frame
